07b00820cad6dc02b0dd210db8f9b7ac78185063
MuhammadHaseebUlHaqq <rajahaseebulhaq@hotmail.com>
2025-12-23 19:39:09 +0500

    Add project metadata package

---
+++ pkg/info/info.go
@@ -0,0 +1,9 @@
+package info
+
+// Project metadata that can be referenced by tooling or documentation.
+const (
+	ProjectName       = "Firaaq"
+	ProjectVersion    = "0.1.0"
+	MaintainerContact = "devops@example.com"
+)
+
